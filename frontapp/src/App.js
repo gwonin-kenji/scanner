0194f313dccd07d2ba57a76237e10e281afe48f0
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2025-04-07 13:08:13 +0000

    make inital frontend page connect to backend

--- frontapp/src/App.js
+++ frontapp/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [domain, setDomain] = useState("");
+  const [whois, setWhois] = useState("");
+
+  const fetchWhois = async () => {
+    const res = await axios.get(`https://backenddomain/whois?domain=${domain}`);
+    setWhois(res.data.whois);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={domain} onChange={(e) => setDomain(e.target.value)} />
+      <button onClick={fetchWhois}>Whois Lookup</button>
+      <pre>{whois}</pre>
     </div>
   );
 }
